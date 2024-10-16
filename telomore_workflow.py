440c26011c8d2a78db2df9aae990c9ffeabef62c
dalofa <dalofa@NBC-shared.win.dtu.dk>
2024-10-16 14:20:30 +0200

    Update merged_workflow file

--- telomore_workflow.py
+++ telomore_workflow.py
@@ -1,15 +1,7 @@
 """Telomore
-Script for finding and extracting telomeres from Illumina reads, which have been excluded/missed from
+Script for finding and extracting telomeres from nanopore or illumina reads, which have been excluded from
 a de novo assembly.
-
-Main differences from main_nanopore:
-- Uses Bowtie2 to map reads
-- Uses Mafft rather than lamassemble to generate consensus
-- Use the quality of reads rather than number and ratio to trim ends ()
 """
-
-
-
 # imports
 import glob
 import argparse
@@ -17,10 +9,15 @@ import os
 import shutil
 import sys
 
+# Nanopore-sepcific imports
+from utlis.cmd_tools import map_and_sort, generate_consensus_lamassemble, train_lastDB
+from utlis.qc_reports import qc_map
+from utlis.map_tools import trim_by_map
+
 # Illumina-specific imports
 from utlis.qc_reports import cons_genome_map, qc_map_illumina, finalize_log
 from utlis.fasta_tools import get_chromosome, strip_fasta
-from utlis.cmd_tools import map_and_sort_illumina, generate_consensus_mafft, map_and_sort
+from utlis.cmd_tools import map_and_sort_illumina, generate_consensus_mafft
 from utlis.map_tools import get_terminal_reads, get_left_soft, get_right_soft, revcomp_reads, revcomp, stich_telo, trim_by_map_illumina
 
 
@@ -38,6 +35,7 @@ def main():
     
     map_out = ref_name+".map.sort.bam"
     if not map_out in folder_content:
+
         if args.mode="nanopore":
             map_and_sort(chrom_out, args.fastq , map_out , args.threads)
         elif args.mode=="illumina":
@@ -46,6 +44,7 @@ def main():
         print("Using already identified .bam-file", map_out)
 
     print("Extracting terminal reads")
+    
     # Select soft-clipped reads that extend the reference
     left_reads = "left.sam"
     right_reads = "right.sam"
@@ -56,68 +55,88 @@ def main():
     get_left_soft(left_reads,left_filt,offset=500) 
     get_right_soft(right_reads,right_filt,offset=500)
 
-    # 1: Generate consensus 
+    # 1: Generate consensus
     # -----------------------------------------------------------------    
-    
-    # Generate left-consensus
+    print("Generating consensus")
+
+    # Generate left_consensus
+
     # To maintain anchor point for alignment, the reads are flipped
     # and the resulting consensus must then be flipped
-    revcomp_reads("left_filtered.fastq","rev_left_filtered.fastq") # flip reads for 
+    revcomp_reads("left_filtered.fastq","rev_left_filtered.fastq")
+    
     l_cons_out="rev_left_cons.fasta"
-    generate_consensus_mafft("rev_left_filtered.fastq",l_cons_out)
     l_cons_final_out="left_cons.fasta"
+
+    if args.mode=="nanopore":
+            db_out=ref_name+".db"
+            train_lastDB(chrom_out,args.fastq,db_out,args.threads)
+            generate_consensus_lamassemble(db_out,"rev_left_filtered.fastq",l_cons_out)
+    elif args.mode=="illumina":
+            generate_consensus_mafft("rev_left_filtered.fastq",l_cons_out)
+    
+    # flip consensus to original original orientation
     revcomp(l_cons_out,l_cons_final_out)
 
+    r_cons_out="right_cons.fasta"
 
     # Generate right-consensus
-    r_cons_out="right_cons.fasta"
-    generate_consensus_mafft("right_filtered.fastq",r_cons_out)
-    print("Consensus generated")
+    if args.mode=="nanopore":
+         generate_consensus_lamassemble(db_out,"right_filtered.fastq",r_cons_out)
+    elif args.mode=="illumina":
+            generate_consensus_mafft("right_filtered.fastq",r_cons_out)
     
-
-
-    # Extend assembly with consensus using and mapping of the consensus
-    # onto the chromosome
-    # discard start or end bases that can provide alternative mapping spots for consensus
+    # 2: Extend assembly with consensus by mapping onto chromsome
+    # -----------------------------------------------------------------    
+    print("Extending assembly")
+    # Discard the bases that could provide alternative sites
+    # for the consensus to map to. Streptomyces typically have TIRs.
     l_map_in= ref_name + ".chrom.left.fa"
-    strip_fasta(chrom_out,l_map_in,500,"end")
-
     r_map_in = ref_name + ".chrom.right.fa"
-    
+    strip_fasta(chrom_out,l_map_in,500,"end")
     strip_fasta(chrom_out,r_map_in ,500,"start")
 
-    # as this is fasta and not illumina, no need to worry
+    # Map onto reduced references
     l_map_out = "left.map.sort.bam"
     map_and_sort(l_map_in,"left_cons.fasta",l_map_out,args.threads,)
-    
     r_map_out = "right.map.sort.bam"
     map_and_sort(r_map_in,"right_cons.fasta",r_map_out,args.threads)
 
+    # Extend the assembly using the map
     stitch_out = ref_name +".01.nontrimmed.cons.fasta"
-    cons_log_out = ref_name + ".ill.cons.log.txt"
+    if args.mode=="nanopore":
+         cons_log_out= ref_name + ".np.cons.log.txt"
+    elif args.mode=="illumina":
+        cons_log_out = ref_name + ".ill.cons.log.txt"
+    
     stich_telo(chrom_out,l_map_out,r_map_out,stitch_out,logout=cons_log_out)
-    print("Consensus attached to genome")
 
-    # 2: Trim consensus
+    # 3: Trim consensus using a map of terminal reads onto extended
+    # assembly
     # ----------------------------------------------------------------- 
-    print("trimming consensus")
+    print("Trimming consensus based on read support")
+    
     trim_map = ref_name + ".01.nontrimmed.map.bam"
-    qc_map_illumina(stitch_out,left_reads,right_reads,trim_map,t=args.threads)
     trim_out = ref_name + ".02.trimmed.cons.fasta"
-    trim_by_map_illumina(stitch_out,trim_map,trim_out, cons_log=cons_log_out, cov_thres=1, ratio_thres=0.7,qual_thres=30)
-    print("Consensus quality trimmed")
+    if args.mode=="nanopore":
+         qc_map(stitch_out,left_reads,right_reads,trim_map,t=args.threads)
+         trim_by_map(stitch_out,trim_map,trim_out, cons_log=cons_log_out, cov_thres=5, ratio_thres=0.7,qual_thres=5)
+    elif args.mode=="illumina": 
+        qc_map_illumina(stitch_out,left_reads,right_reads,trim_map,t=args.threads)
+        trim_by_map_illumina(stitch_out,trim_map,trim_out, cons_log=cons_log_out, cov_thres=1, ratio_thres=0.7,qual_thres=30)
 
-    # 3: QC and clean-up
+    # 4: Generate QC files
     # -----------------------------------------------------------------
+    print("Generate QC map and finalize result-log")
+    
     qc_out = ref_name + ".02.reads.trimmed.map.bam"
     qc_map_illumina(trim_out,left_reads,right_reads,qc_out,t=args.threads)
     cons_genome_map_out = ref_name + ".02.cons.trimmed.map.bam"
     cons_genome_map("left_cons.fasta","right_cons.fasta",trim_out,cons_genome_map_out,t=args.threads)
-    #cons_cons_map_out=ref_name+".03.cons_vs_cons.map"
-    #cons_cons_map("left_cons.fasta","right_cons.fasta",cons_cons_map_out,t=args.threads)
     finalize_log(cons_log_out,"tmp.right.fasta","tmp.left.fasta")
-    print("QC report and alignments generated")
-
+    
+    # 5: Clean-up
+    # -----------------------------------------------------------------
     #rm all the files that were made
     if args.keep==False:
     # all bam-files

9ff55b3d8676ebd1d10319133cb6fb6d998c9160
Tue Sparholt Jørgensen <54983329+tuspjo@users.noreply.github.com>
2024-10-21 13:54:17 +0200

    Update bowtie2_cmd.sh

--- bash_scripts/bowtie2_cmd.sh
+++ bash_scripts/bowtie2_cmd.sh
@@ -12,7 +12,7 @@ bowtie2-build -q --threads $THREADS $REF $REF.bt.index
 
 #-a to get secondary and supplementary alignments
 # Local to allow soft-clips that extends beyond the genome edge (as opposed to end-to-end)
-bowtie2  -a -p $THREADS --local --sam-no-qname-trunc -x $REF.bt.index -1 $READ1 -2 $READ2 -S $OUTPUT --quiet
+bowtie2  -a -p $THREADS --local --sam-no-qname-trunc -x $REF.bt.index -1 $READ1 -2 $READ2 -S $OUTPUT --quiet --no-mixed --no-discordant
 
 # sort map file and assign it the intended output name
 samtools sort -@ $THREADS $OUTPUT > $OUTPUT.sort.bam

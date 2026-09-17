1dc12cb787e3727182fa8498ee486dc4924ef79e
David Faurdal <83669966+dalofa@users.noreply.github.com>
2026-09-17 13:34:12 +0200

    Fix edge_case, where multiple mappings of consensus prevented extension (#63)
    * Fix edge_case, where multiple mappings of consensus prevented extension
    
    * Style fixes by Ruff
    
    ---------
    
    Co-authored-by: dalofa <dalofa@nisin.win.dtu.dk>

--- src/telomore/utils/map_tools.py
+++ src/telomore/utils/map_tools.py
@@ -813,10 +813,7 @@ def stitch_telo(
                 clip_num = int(lmatch.group(1))  # digits are retrieve via .group
 
                 # check if the clipped sequence extends beyond genome
-                if clip_num - read.reference_start <= 0:
-                    left_log_mes = f'#The consensus produced for the left-side does extend beyond the start of {ref}'
-                    left_seqs = []
-                else:
+                if clip_num - read.reference_start > 0:  # This means it is extending
                     seq = read.query_sequence[
                         0 : (clip_num - read.reference_start)
                     ]  # Adjust for if more than just overhanging bases are soft-clipped
